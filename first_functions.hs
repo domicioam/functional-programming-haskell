6cbfca9dcd1c74321d0f6e4b70f116b6057385c7
Domício de Araújo Medeiros <domcio@pop-os.localdomain>
2021-07-11 11:42:20 +0100

    guard clauses, fizzbuzz

--- first_functions.hs
+++ first_functions.hs
@@ -5,3 +5,21 @@ sayTwice s = s ++ s
 
 cube :: Int -> Int
 cube n = n * n * n
+
+sign :: Int -> Int
+sign n 
+    | n > 0 = 1
+    | n < 0 = -1
+    | n == 0 = 0
+
+isPalindrome :: String -> Bool
+isPalindrome s 
+    | reverse s == s = True
+    | otherwise      = False
+
+fizzbuzz :: Int -> String
+fizzbuzz i
+    | mod i 5 == 0 && mod i 3 /= 0 = "buzz"
+    | mod i 3 == 0 && mod i 5 /= 0 = "fizz"
+    | mod i 3 == 0 && mod i 5 == 0 = "fizzbuzz"   
+    | otherwise = show i
